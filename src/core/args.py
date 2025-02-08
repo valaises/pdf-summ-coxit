0db87962b5f8e22a01ba302a4d4f5b94bd239524
V4LER11 <work@valerii.cc>
2025-02-08 10:58:40 +0000

    created args.py

---
+++ src/core/args.py
@@ -0,0 +1,39 @@
+import sys
+
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+
+
+from logger import init_logger, info, error
+
+
+__all__ = ["parse_args"]
+
+
+@dataclass
+class Args:
+    target_dir: Path
+    DEBUG: bool
+
+
+def parse_args(base_dir: Path) -> Args:
+    init_logger(True)
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--target_dir", required=True, type=Path, help="Directory to watch for new files")
+    parser.add_argument("--DEBUG", default=False, action="store_true")
+    args = parser.parse_args()
+
+    target_dir: Path = args.target_dir
+    if not target_dir.is_absolute():
+        target_dir = base_dir / target_dir
+        info(f"target dir '{args.target_dir}' -> '{target_dir}' (completed to absolute path)")
+
+    if not target_dir.is_dir():
+        error(f"Target Directory '{target_dir}' does not exist")
+        sys.exit(1)
+
+    return Args(
+        target_dir=target_dir,
+        DEBUG=args.DEBUG
+    )
--- src/core/main.py
+++ src/core/main.py
@@ -1,8 +1,7 @@
-import sys
-from argparse import ArgumentParser
 from pathlib import Path
 
-from logger import init_logger, info, error
+from args import parse_args
+from logger import init_logger, info
 from pdf_watchdog import start_watchdog
 
 
@@ -10,22 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("-d", "--target_dir", required=True, type=Path, help="Directory to watch for new files")
-    parser.add_argument("--DEBUG", default=False, action="store_true")
-    args = parser.parse_args()
-
+    init_logger(True)
+    args = parse_args(BASE_DIR)
     init_logger(args.DEBUG)
     info("logger initialized")
-
-    target_dir: Path = args.target_dir
-    if not target_dir.is_absolute():
-        target_dir = BASE_DIR / target_dir
-        info(f"target dir '{args.target_dir}' -> '{target_dir}' (completed to absolute path)")
-
-    if not target_dir.is_dir():
-        error(f"Target Directory '{args.target_dir}' does not exist")
-        sys.exit(1)
 
     q, stop_event = start_watchdog(args.target_dir)
     try:
